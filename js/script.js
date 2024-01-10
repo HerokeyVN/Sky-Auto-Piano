033748eeb9289a6161944e55d798e9f02c0a4274
HerokeyVN <70248685+HerokeyVN@users.noreply.github.com>
2024-01-10 17:32:42 +0700

    Update GUI

--- js/script.js
+++ js/script.js
@@ -1,4 +1,6 @@
-const { ipcRenderer } = require('electron');
+const { ipcRenderer, globalShortcut } = require('electron');
+// const globalShortcut = remote.globalShortcut;
+console.log(globalShortcut)
 const fs = require("fs");
 const path = require("path");
 const { Base64 } = require('js-base64');
@@ -6,15 +8,25 @@ const keys = ["y", "u", "i", "o", "p",
     "h", "j", "k", "l", ";",
     "n", "m", ",", ".", "/"];
 var listSheet = JSON.parse(fs.readFileSync(path.join(__dirname, "..", "data", "listSheet.json"), { encoding: "utf8" }));
+var listKeys = [];
+var playing = 0;
+var isPlay = false;
+var maxPCB = 0;
 
 // Load card
 printSheet();
+
+if (listSheet.length > 0) {
+    !listKeys[0] ? listKeys[0] = JSON.parse(fs.readFileSync(path.join(__dirname, "..", "data", listSheet[0].keyMap), { encoding: "utf8" })) : "";
+
+    updateFooter({ ...listSheet[0], keys: listKeys[0] }, 0);
+}
 
 // Add sheet
 document.getElementsByClassName('btn-add')[0].addEventListener('change', (event) => {
     const { files } = event.target;
     let repl = fs.readFileSync(path.join(__dirname, "..", "js", "repl"));
-    console.log(repl);
+    //console.log(repl);
     let text = fs.readFileSync(files[0].path, { encoding: "utf8" }).replaceAll("��", '').replaceAll(repl, '');
     //console.log(eval(text).name);
     let json = eval(text)[0];
@@ -27,14 +39,14 @@ function encSheet(json) {
     if (json.isEncrypted) return;
     let tempEnc = {};
     //let fileName = Base64.encode((new Date()).getTime+"")+'.json';
-    let fileName = Base64.encode(random(1, 9999) + (json.name+"").replace(/[^a-zA-Z0-9]/g, '-')) + '.json';
+    let fileName = Base64.encode(random(1, 9999) + (json.name + "").replace(/[^a-zA-Z0-9]/g, '-')) + '.json';
     for (let j in json.songNotes) {
         let i = json.songNotes[j]
         !tempEnc[i.time] ? tempEnc[i.time] = [] : "";
         tempEnc[i.time].push(keys[parseInt(i.key.split("Key")[1])]);
     }
 
-    fs.writeFileSync(path.join(__dirname, "..", "data", fileName), JSON.stringify(tempEnc), { mode: 0o666 });
+    fs.writeFileSync(path.join(__dirname, "..", "data", fileName), JSON.stringify(!tempEnc["0"] ? {"0": [], ...tempEnc}:tempEnc), { mode: 0o666 });
     listSheet.push({
         name: json.name,
         author: json.author,
@@ -65,9 +77,9 @@ function printSheet() {
             <a class="bpm-sheet">BPM: ${i.bpm}<br></a>
         </div>
         <div class="menu-btn">
-            <svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" fill="currentColor" class="bi bi-play-fill" viewBox="0 0 16 16">
+            <!--svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" fill="currentColor" class="bi bi-play-fill" viewBox="0 0 16 16">
                 <path d="m11.596 8.697-6.363 3.692c-.54.313-1.233-.066-1.233-.697V4.308c0-.63.692-1.01 1.233-.696l6.363 3.692a.802.802 0 0 1 0 1.393z"/>
-            </svg>
+            </svg-->
             <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-trash3" viewBox="0 0 16 16">
                 <path d="M6.5 1h3a.5.5 0 0 1 .5.5v1H6v-1a.5.5 0 0 1 .5-.5M11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H2.506a.58.58 0 0 0-.01 0H1.5a.5.5 0 0 0 0 1h.538l.853 10.66A2 2 0 0 0 4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1h-.995a.59.59 0 0 0-.01 0zm1.958 1-.846 10.58a1 1 0 0 1-.997.92h-6.23a1 1 0 0 1-.997-.92L3.042 3.5zm-7.487 1a.5.5 0 0 1 .528.47l.5 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1 .47-.53Zm5.058 0a.5.5 0 0 1 .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47ZM8 4.5a.5.5 0 0 1 .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5" />
             </svg>
@@ -77,16 +89,135 @@ function printSheet() {
 
     for (let j in listSheet) {
         let i = listSheet[j];
-        let btnPlay = document.getElementsByClassName("bi-play-fill");
-        btnPlay[j].onclick = ()=>{
-            ipcRenderer.send("play", i.keyMap);
+        let btnPlay = document.getElementsByClassName("card");
+        btnPlay[j].onclick = () => {
+            //ipcRenderer.send("play", i.keyMap);
+            !listKeys[j] ? listKeys[j] = JSON.parse(fs.readFileSync(path.join(__dirname, "..", "data", i.keyMap), { encoding: "utf8" })) : "";
+
+            updateFooter({ ...listSheet[j], keys: listKeys[j] }, j);
         }
 
         let btndel = document.getElementsByClassName("bi-trash3");
-        btndel[j].onclick = ()=>{
+        btndel[j].onclick = () => {
             fs.unlinkSync(path.join(__dirname, "..", "data", listSheet[j].keyMap));
             listSheet.splice(j, 1);
+            listKeys.splice(j, 1);
+            fs.writeFileSync(path.join(__dirname, "..", "data", "listSheet.json"), JSON.stringify(listSheet, null, 4), { mode: 0o666 });
             printSheet();
         }
     }
+}
+
+function updateFooter(info, id) {
+    playing = id;
+    let delayMap = Object.keys(info.keys)
+    document.getElementsByClassName('process-bar')[0].max = Math.trunc(Number(delayMap[delayMap.length - 1]) / (1000));
+    maxPCB = Math.trunc(Number(delayMap[delayMap.length - 1]) / (1000));
+    document.getElementsByClassName('name-playing')[0].innerHTML = info.name;
+    document.getElementsByClassName('process-bar')[0].value = 0;
+    document.getElementsByClassName('live-time')[0].innerHTML = `00:00`
+    //console.log(Number(delayMap[delayMap.length - 1]) / 1000);
+    let totalMin = Math.trunc(Number(delayMap[delayMap.length - 1]) / (60 * 1000));
+    totalMin < 10 ? totalMin = "0" + (totalMin + "") : "";
+    let totalSec = Math.trunc(Number(delayMap[delayMap.length - 1]) / (1000)) - (totalMin * 60);
+    totalSec < 10 ? totalSec = "0" + (totalSec + "") : "";
+    document.getElementsByClassName('total-time')[0].innerHTML = `${totalMin}:${totalSec}`
+}
+
+function btnPrev() {
+    if (playing-1 < 0) playing = listSheet.length-1;
+    else playing--;
+    !listKeys[playing] ? listKeys[playing] = JSON.parse(fs.readFileSync(path.join(__dirname, "..", "data", listSheet[playing].keyMap), { encoding: "utf8" })) : "";
+    updateFooter({ ...listSheet[playing], keys: listKeys[playing] }, playing);
+
+    if (isPlay) {
+        btnPlay();
+        document.getElementsByClassName('process-bar')[0].value = 0;
+        document.getElementsByClassName('live-time')[0].innerHTML = `00:00`;
+        setTimeout(btnPlay, 1000);
+    }
+}
+
+document.getElementById('btn-prev').addEventListener("click", btnPrev);
+ipcRenderer.on('btn-prev', btnPrev);
+
+function btnNext() {
+    if (playing+1 >= listSheet.length) playing = 0;
+    else playing++;
+    !listKeys[playing] ? listKeys[playing] = JSON.parse(fs.readFileSync(path.join(__dirname, "..", "data", listSheet[playing].keyMap), { encoding: "utf8" })) : "";
+    updateFooter({ ...listSheet[playing], keys: listKeys[playing] }, playing);
+
+    if (isPlay) {
+        btnPlay();
+        document.getElementsByClassName('process-bar')[0].value = 0;
+        document.getElementsByClassName('live-time')[0].innerHTML = `00:00`;
+        setTimeout(btnPlay, 1000);
+    }
+}
+document.getElementById('btn-next').addEventListener("click", btnNext);
+ipcRenderer.on('btn-next', btnNext);
+
+function btnPlay() {
+    isPlay = isPlay ? false : true;
+    let send = {
+        keys: sec2array(Number(document.getElementsByClassName('process-bar')[0].value), listKeys[playing]),
+        sec: Number(document.getElementsByClassName('process-bar')[0].value)+1
+    }
+    ipcRenderer.send("play", send);
+    document.getElementsByClassName('process-bar')[0].disabled = isPlay ? true:false;
+    document.getElementById('btn-play').innerHTML = isPlay ? `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pause-fill" viewBox="0 0 16 16">
+    <path d="M5.5 3.5A1.5 1.5 0 0 1 7 5v6a1.5 1.5 0 0 1-3 0V5a1.5 1.5 0 0 1 1.5-1.5m5 0A1.5 1.5 0 0 1 12 5v6a1.5 1.5 0 0 1-3 0V5a1.5 1.5 0 0 1 1.5-1.5"/>
+    </svg>
+    Pause (Shift+V)`: `<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-play-fill" viewBox="0 0 16 16">
+    <path d="m11.596 8.697-6.363 3.692c-.54.313-1.233-.066-1.233-.697V4.308c0-.63.692-1.01 1.233-.696l6.363 3.692a.802.802 0 0 1 0 1.393z"/>
+    </svg>
+    Play (Shift+V)`;
+}
+
+document.getElementById('btn-play').addEventListener("click", btnPlay);
+ipcRenderer.on('btn-play', btnPlay);
+
+ipcRenderer.on("process-bar", (event, data)=>{
+	document.getElementsByClassName('process-bar')[0].value = data;
+    //console.log(data);
+    let s2m = sec2min(Number(data));
+    let min = s2m.min < 10 ? "0"+(s2m.min+""):s2m.min;
+    let sec = s2m.sec < 10 ? "0"+(s2m.sec+""):s2m.sec;
+    document.getElementsByClassName('live-time')[0].innerHTML = `${min}:${sec}`
+})
+ipcRenderer.on("stop-player", (event, data)=>{
+	document.getElementById('btn-play').innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="20" heipkihght="20" fill="currentColor" class="bi bi-play-fill" viewBox="0 0 16 16">
+    <path d="m11.596 8.697-6.363 3.692c-.54.313-1.233-.066-1.233-.697V4.308c0-.63.692-1.01 1.233-.696l6.363 3.692a.802.802 0 0 1 0 1.393z"/>
+    </svg>
+    Play (Shift+V)`;
+    isPlay = false;
+    document.getElementById('process-bar').disabled = isPlay ? true:false;
+    document.getElementsByClassName('process-bar')[0].value = 0;
+    document.getElementsByClassName('live-time')[0].innerHTML = '00:00';
+})
+
+document.getElementById('process-bar').addEventListener('change', (data)=>{
+    document.getElementsByClassName('process-bar')[0].max = maxPCB;
+    //console.log(document.getElementsByClassName('process-bar')[0].value);
+    let s2m = sec2min(Number(data.target.value));
+    let min = s2m.min < 10 ? "0"+(s2m.min+""):s2m.min;
+    let sec = s2m.sec < 10 ? "0"+(s2m.sec+""):s2m.sec;
+    document.getElementsByClassName('live-time')[0].innerHTML = `${min}:${sec}`;
+})
+
+function sec2min(sec) {
+    let res = {
+        min: Math.trunc(sec/60),
+        sec: sec - Math.trunc(sec/60)*60
+    }
+    return res;
+}
+
+function sec2array(sec, arr) {
+    let res = { ...arr };
+    for (let i in arr) {
+        if (Number(i) < sec*1000) delete res[i];
+    }
+
+    return res;
 }
--- main.js
+++ main.js
@@ -1,33 +1,67 @@
-import { app, BrowserWindow, ipcMain } from 'electron/main';
+import { app, BrowserWindow, globalShortcut, Menu, ipcMain } from 'electron/main';
 import fs from "fs";
 import path from 'node:path';
 import { Hardware } from "keysender";
-import {fileURLToPath} from 'url';
+import { fileURLToPath } from 'url';
+import { moveMessagePortToContext } from 'worker_threads';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const keys = ["y", "u", "i", "o", "p",
-              "h", "j", "k", "l", ";",
-              "n", "m", ",", ".", "/"];
+  "h", "j", "k", "l", ";",
+  "n", "m", ",", ".", "/"];
+var isPlay = false;
+var interval;
 
-function createWindow () {
+Menu.setApplicationMenu(Menu.buildFromTemplate([]))
+
+function createWindow() {
   const win = new BrowserWindow({
-    width: 800,
+    width: 750,
+    minWidth: 650,
     height: 600,
+    minHeight: 185,
     backgroundColor: "#0a1930",
     // transparent: true,
     // frame: false,
     webPreferences: {
       nodeIntegration: true,
-			contextIsolation: false
+      contextIsolation: false
     }
   })
 
   win.loadFile(path.join(__dirname, 'index', 'index.html'));
 
+
+
   ipcMain.on("play", (event, data) => {
-    var keyMap = JSON.parse(fs.readFileSync(path.join(__dirname, "data", data), { encoding: "utf8" }));
-    autoPlay(keyMap);
-	})
+    //var keyMap = JSON.parse(fs.readFileSync(path.join(__dirname, "data", data), { encoding: "utf8" }));
+    
+    isPlay = !isPlay ? true:false;
+    isPlay ? win.minimize():"";
+    let mapDelay = Object.keys(data.keys);
+    autoPlay(data.keys);
+    sendTimeProcess(Number(mapDelay[mapDelay.length-1]), data.sec);
+  })
+
+  async function sendTimeProcess(total, sec) {
+    for(let i = sec; i<=Math.trunc(total / (1000)); i++) {
+      await new Promise((rev) => setTimeout(rev, 1*1000));
+      if (!isPlay) return;
+      win.webContents.send("process-bar", i);
+    }
+    win.webContents.send("stop-player");
+    isPlay = false;
+  }
+
+  globalShortcut.register('Shift+C', () => {
+    win.webContents.send("btn-prev");
+  })
+  globalShortcut.register('Shift+V', () => {
+    win.webContents.send("btn-play");
+  })
+  globalShortcut.register('Shift+B', () => {
+    win.webContents.send("btn-next");
+  })
 }
 
 app.whenReady().then(() => {
@@ -46,19 +80,22 @@ app.on('window-all-closed', () => {
   }
 })
 
-async function autoPlay(keyMap){
+async function autoPlay(keyMap) {
   let ks = (new Hardware("Sky")).keyboard;
   let objKey = Object.keys(keyMap);
-  await new Promise((rev)=>setTimeout(rev, 3.5*1000));
+  await new Promise((rev) => setTimeout(rev, 0.5 * 1000));
 
-  for (let i=1; i < objKey.length; i++) {
-      let delay = objKey[i] - objKey[i-1];
+  for (let i = 1; i < objKey.length; i++) {
+    let delay = objKey[i] - objKey[i - 1];
 
-      console.log(keyMap[objKey[i-1]]);
-    for (let j of keyMap[objKey[i-1]]) ks.sendKeys(j);
+    if (!isPlay) break;
+    console.log(keyMap[objKey[i - 1]]);
+    for (let j of keyMap[objKey[i - 1]]) ks.sendKeys(j);
 
-      await new Promise((rev)=>setTimeout(rev, delay));
+    await new Promise((rev) => setTimeout(rev, delay));
   }
-  console.log(keyMap[objKey[objKey.length-1]]);
-  for (let j of keyMap[objKey[objKey.length-1]]) ks.sendKeys(j);
+  if (!isPlay) return;
+  console.log(keyMap[objKey[objKey.length - 1]]);
+  for (let j of keyMap[objKey[objKey.length - 1]]) ks.sendKeys(j);
 }
+
